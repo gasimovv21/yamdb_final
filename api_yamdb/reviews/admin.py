e150c8981b9eba7ef4faea7a4d8f0e2ba246ffc8
Eltun Gasimov <framee21@yandex.com>
2022-10-10 01:20:04 +0200

    Fix 3

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,9 +1,9 @@
-from .models import User, Category, Genre, Title, Review, Comment
 from django.contrib import admin
+from .models import Category, Comment, Genre, Review, Title, User
 
-admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Title)
-admin.site.register(Review)
 admin.site.register(Comment)
+admin.site.register(Genre)
+admin.site.register(Review)
+admin.site.register(Title)
+admin.site.register(User)
