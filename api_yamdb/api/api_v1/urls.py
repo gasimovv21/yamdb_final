cee917e2a7704aa7a584ec905b2e6fd22d7aed09
Eltun Gasimov <framee21@yandex.com>
2022-10-10 01:40:19 +0200

    fix 5

--- api_yamdb/api/api_v1/urls.py
+++ api_yamdb/api/api_v1/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CategoryViewSet, TitleViewSet, GenreViewSet,
-                    ReviewViewSet, CommentViewSet, UserViewSet,
-                    get_jwt_token, register)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet, get_jwt_token,
+                    register)
 
 routerv1 = DefaultRouter()
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
-from reviews.validators import validate_date
 from django.db import models
+from reviews.validators import validate_date
 
 
 class User(AbstractUser):
